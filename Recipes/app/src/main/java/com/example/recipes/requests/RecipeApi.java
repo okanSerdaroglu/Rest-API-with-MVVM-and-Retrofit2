b64079b78a4365d26bf85e5e7ae63a06c2c200ed
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-18 22:14:43 +0300

    recipeApi get methods

--- Recipes/app/src/main/java/com/example/recipes/requests/RecipeApi.java
+++ Recipes/app/src/main/java/com/example/recipes/requests/RecipeApi.java
@@ -1,9 +1,28 @@
 package com.example.recipes.requests;
 
 
+import com.example.recipes.requests.responses.RecipeResponse;
+import com.example.recipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RecipeApi {
 
+    // search method
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
 
+    // get recipe request
+    @GET("api/get")
+    Call<RecipeResponse>getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipeId
+    );
 
 }
