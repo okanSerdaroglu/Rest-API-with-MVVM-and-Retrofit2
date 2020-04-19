5cf81f9e878290f89eb59b658f9e3bb22f6fbfab
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-19 17:33:57 +0300

    executors for background task

--- Recipes/app/src/main/java/com/example/recipes/requests/RecipeApiClient.java
+++ Recipes/app/src/main/java/com/example/recipes/requests/RecipeApiClient.java
@@ -3,9 +3,13 @@ package com.example.recipes.requests;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.recipes.AppExecutors;
 import com.example.recipes.models.Recipe;
+import com.example.recipes.utils.Constants;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class RecipeApiClient {
 
@@ -26,4 +30,17 @@ public class RecipeApiClient {
     public LiveData<List<Recipe>> getRecipes() {
         return mRecipes;
     }
+
+    public void searchRecipesApi() {
+        final Future handler = AppExecutors.getInstance().networkIO().submit(() -> {
+            // retrieve data from rest API
+            // mRecipes.postValue();
+        });
+        AppExecutors.getInstance().networkIO().schedule(() -> {
+            // let the user know its time out
+            handler.cancel(true);
+
+        }, Constants.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
 }
--- Recipes/app/src/main/java/com/example/recipes/utils/Constants.java
+++ Recipes/app/src/main/java/com/example/recipes/utils/Constants.java
@@ -3,5 +3,6 @@ package com.example.recipes.utils;
 public class Constants {
 
     public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    public static final int NETWORK_TIMEOUT = 3000;
 
 }
